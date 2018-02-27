f42b860b488c7722d42e3e0b02fe40bba14dc5d2
JChus7 <77chus77@gmail.com>
2018-02-27 12:05:05 +0100

    Separacion del constructor del laberinto
    Se ha separado la parte que construye el laberinto de la que controla el curso del juego.
    También se han eliminado prefabs obsoletos.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,17 +11,13 @@ public class GameManager : MonoBehaviour
     public int m_NumTurnos;
     public float m_StartDelay = 3f;
     public float m_EndDelay = 3f;
-    public GameObject m_Tile;
-    public GameObject m_Border;
-    public GameObject m_Wall;
-    public GameObject[] m_SpawnList = new GameObject[4];
     public MouseManager[] m_Mouse;
     public GameObject m_MousePrefab;
 
 
     private WaitForSeconds m_StartWait;
     private WaitForSeconds m_EndWait;
-    private static int m_MazeLength = 10;
+
     
 
 
@@ -33,90 +29,15 @@ public class GameManager : MonoBehaviour
         m_StartWait = new WaitForSeconds(m_StartDelay);
         m_EndWait = new WaitForSeconds(m_EndDelay);
 
-        CreateMaze();
+       
         //SpawnAllMouses();
 
         //StartCoroutine(GameLoop());
     }
 
-    private void CreateMaze()
-    {
-       
-        int rZ = 0;  //eje z rotation
+   
 
-        var maze = Maze.Load();
-        Vector3 newPosition = new Vector3();
-        Quaternion newRotation = new Quaternion();
-        //Instance the tiles of the maze
-        int contSpawns = 0;
-        for (int cont = 0; cont < m_MazeLength*m_MazeLength; cont++)
-        {
-           
-            
-                newPosition.x = maze.Tiles[cont].X * 10;
-                newPosition.z = maze.Tiles[cont].Z * 10;
-
-                GameObject tile = Instantiate(m_Tile, newPosition, newRotation);
-                tile.GetComponent<TileManager>().SetPosition(maze.Tiles[cont].X, maze.Tiles[cont].Z);
-               
-                switch (maze.Tiles[cont].Contains)
-                {
-                    case "Empty": break;
-                    case "Spawn":
-                       
-                        newPosition.y = 2.5f;
-                        m_SpawnList[contSpawns].transform.SetPositionAndRotation(newPosition, newRotation);
-                        contSpawns++;
-                        newPosition.y = 0f;
-                        break;
-                    case "Wall":
-                        newPosition.y = 5f;
-                        newRotation = Quaternion.Euler(0, maze.Tiles[cont].Rotation, 0);
-                        GameObject wall = Instantiate(m_Wall, newPosition, newRotation);
-                        MeshRenderer[] renderers = wall.GetComponentsInChildren<MeshRenderer>();
-
-                        for (int i = 0; i < renderers.Length; i++)
-                        {
-                            // ... set their material color to the color specific to this tank.
-                            renderers[i].material.color = Color.gray;
-                        }
-                        tile.GetComponent<TileManager>().SetContains(wall);
-                        newPosition.y = 0f;
-                        break;
-                }
-               
-                newRotation = Quaternion.Euler(0, 0, 0);
-                newPosition.y = 0f;
-            
-        }
-        //Instance the walls of the maze
-        newPosition.y = 5;
-
-        newPosition.x = (m_MazeLength * 10 / 2) - 5;
-        newPosition.z = -5;
-        newRotation = Quaternion.Euler(90f, 0f, rZ);
-        Instantiate(m_Border, newPosition, newRotation);
-
-        rZ = 270;
-        newPosition.x = -5;
-        newPosition.z = (m_MazeLength * 10 / 2) - 5;
-        newRotation = Quaternion.Euler(90f, 0f, rZ);
-        Instantiate(m_Border, newPosition, newRotation);
-
-        rZ = 180;
-        newPosition.x = (m_MazeLength * 10 / 2) - 5;
-        newPosition.z = (m_MazeLength * 10) - 5;
-        newRotation = Quaternion.Euler(90f, 0f, rZ);
-        Instantiate(m_Border, newPosition, newRotation);
-
-        rZ = 90;
-        newPosition.x = (m_MazeLength * 10) - 5;
-        newPosition.z = (m_MazeLength * 10 / 2) - 5;
-        newRotation = Quaternion.Euler(90f, 0f, rZ);
-        Instantiate(m_Border, newPosition, newRotation);
-    }
-
-    private void SpawnAllMouses()
+    /*private void SpawnAllMouses()
     {
 
         for (int i = 0; i < m_Mouse.Length; i++)
@@ -131,7 +52,7 @@ public class GameManager : MonoBehaviour
             m_Mouse[i].Setup();
         }
 
-    }
+    }*/
 
     // Update is called once per frame
     private IEnumerator GameLoop()
---
+++ Assets/Scripts/MazeBuilder.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using UnityEngine;
+
+public class MazeBuilder: MonoBehaviour {
+
+
+
+	public GameObject m_Tile;
+	public GameObject m_Border;
+	public GameObject m_Wall;
+	public GameObject[] m_SpawnList = new GameObject[4];
+	private static int m_MazeLength = 10;
+
+
+
+	void Start(){
+		int rZ = 0;  //eje z rotation
+
+		var maze = Maze.Load();
+		Vector3 newPosition = new Vector3();
+		Quaternion newRotation = new Quaternion();
+		//Instance the tiles of the maze
+		int contSpawns = 0;
+		for (int cont = 0; cont < m_MazeLength*m_MazeLength; cont++)
+		{
+
+
+			newPosition.x = maze.Tiles[cont].X * 10;
+			newPosition.z = maze.Tiles[cont].Z * 10;
+
+			GameObject tile = Instantiate(m_Tile, newPosition, newRotation);
+			tile.GetComponent<TileManager>().SetPosition(maze.Tiles[cont].X, maze.Tiles[cont].Z);
+
+			switch (maze.Tiles[cont].Contains)
+			{
+			case "Empty": break;
+			case "Spawn":
+
+				newPosition.y = 2.5f;
+				m_SpawnList[contSpawns].transform.SetPositionAndRotation(newPosition, newRotation);
+				contSpawns++;
+				newPosition.y = 0f;
+				break;
+			case "Wall":
+				newPosition.y = 5f;
+				newRotation = Quaternion.Euler(0, maze.Tiles[cont].Rotation, 0);
+				GameObject wall = Instantiate(m_Wall, newPosition, newRotation);
+				MeshRenderer[] renderers = wall.GetComponentsInChildren<MeshRenderer>();
+
+				for (int i = 0; i < renderers.Length; i++)
+				{
+					// ... set their material color to the color specific to this tank.
+					renderers[i].material.color = Color.gray;
+				}
+				tile.GetComponent<TileManager>().SetContains(wall);
+				newPosition.y = 0f;
+				break;
+			}
+
+			newRotation = Quaternion.Euler(0, 0, 0);
+			newPosition.y = 0f;
+
+		}
+		//Instance the walls of the maze
+		GameObject border;
+		Vector3 scale = new Vector3(m_MazeLength,1,1);
+		newPosition.y = 5;
+
+		newPosition.x = (m_MazeLength * 10 / 2) - 5;
+		newPosition.z = -5;
+		newRotation = Quaternion.Euler(90f, 0f, rZ);
+
+		border = Instantiate(m_Border, newPosition, newRotation);
+        border.transform.localScale = scale;
+
+		rZ = 270;
+		newPosition.x = -5;
+		newPosition.z = (m_MazeLength * 10 / 2) - 5;
+		newRotation = Quaternion.Euler(90f, 0f, rZ);
+
+		border = Instantiate(m_Border, newPosition, newRotation);
+        border.transform.localScale = scale;
+
+        rZ = 180;
+		newPosition.x = (m_MazeLength * 10 / 2) - 5;
+		newPosition.z = (m_MazeLength * 10) - 5;
+		newRotation = Quaternion.Euler(90f, 0f, rZ);
+
+		border = Instantiate(m_Border, newPosition, newRotation);
+        border.transform.localScale = scale;
+
+        rZ = 90;
+		newPosition.x = (m_MazeLength * 10) - 5;
+		newPosition.z = (m_MazeLength * 10 / 2) - 5;
+		newRotation = Quaternion.Euler(90f, 0f, rZ);
+
+		border = Instantiate(m_Border, newPosition, newRotation);
+        border.transform.localScale = scale;
+    }
+		
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
